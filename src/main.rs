5af9d966ac9c670315f0243002bd684aa20755b9
AmanKhys <amankhys2112@gmail.com>
2025-05-09 09:25:20 +0530

    make working make_tree fn; remove make_tree method; change it's working;

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,49 @@
 #[derive(Clone)]
-
 struct Node {
     id: i32,
     children: Vec<Node>,
 }
+
 impl Node {
-    fn add_child(&mut self, child: &Node) {
-        let c = child.clone();
-        self.children.push(c);
+    fn add_child(&mut self, child: Node) -> *mut Node {
+        self.children.push(child);
+        self.children.last_mut().unwrap() as *mut Node
     }
-    fn make_tree(&mut self, layer: i32) {
-        let mut current_layer_nodes: Vec<*mut Node> = vec![self];
+}
+
+fn make_tree(layer: i32) -> Node {
+    let mut id = 1;
+    let mut root = Node {
+        id,
+        children: vec![],
+    };
+    id = id + 1;
+    let mut current_layer_nodes: Vec<*mut Node> = vec![&mut root];
+    let pi_values = take_pie_values(layer);
+
+    for i in 1..pi_values.len() {
         let mut next_layer_nodes: Vec<*mut Node> = vec![];
-        let pi_values = take_pie_values(layer);
-        // using .len() to make the iterable a usize type
-        // instead of layer directly
-        for i in 0..pi_values.len() {
-            for &node in &current_layer_nodes {
-                for j in 1..=pi_values[i] {
-                    let mut child: Node = Node {
-                        id: j,
-                        children: vec![],
-                    };
-                    unsafe {
-                        (*node).add_child(&child);
-                    }
-                    next_layer_nodes.push(&mut child);
+        for &node_ptr in &current_layer_nodes {
+            for _ in 1..=pi_values[i] {
+                let child = Node {
+                    id,
+                    children: vec![],
+                };
+                unsafe {
+                    let child_ptr = (*node_ptr).add_child(child);
+                    next_layer_nodes.push(child_ptr);
                 }
+                id = id + 1;
             }
-            current_layer_nodes = next_layer_nodes.clone();
-            next_layer_nodes = vec![];
         }
+        current_layer_nodes = next_layer_nodes;
     }
+    return root;
 }
 
 fn print_tree(root: Node) {
     let mut queue = vec![root];
-    while !queue.is_empty() {
-        let node = queue.remove(0);
+    while let Some(node) = queue.pop() {
         println!("Node ID: {}", node.id);
         queue.extend(node.children);
     }
@@ -45,24 +51,18 @@ fn print_tree(root: Node) {
 
 fn take_pie_values(len: i32) -> Vec<i32> {
     let pi = std::f64::consts::PI;
-    let mut nums: Vec<i32> = vec![];
+    let mut nums = vec![];
     let multiplier = i32::pow(10, len as u32);
     let mut int_pi = (pi * multiplier as f64) as i32;
-    println!("{}", int_pi);
     for _ in 0..len {
-        let v = int_pi % 10;
-        int_pi = int_pi / 10;
-        nums.push(v);
+        nums.push(int_pi % 10);
+        int_pi /= 10;
     }
     nums.reverse();
-    return nums;
+    nums
 }
 
 fn main() {
-    let mut root = Node {
-        id: 32,
-        children: vec![],
-    };
-    root.make_tree(5);
-    print_tree(root);
+    let tree = make_tree(4);
+    print_tree(tree);
 }
